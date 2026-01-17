6d3123ee79767f26523d0a04639b8e72edfec29a
Utlwang Mooka <123069417+Braidy1@users.noreply.github.com>
2026-01-17 20:13:19 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const toggle = document.getElementById("modeToggle");
-  toggle.addEventListener("click", () => {
-    document.documentElement.dataset.theme =
-      document.documentElement.dataset.theme === "light" ? "dark" : "light";
-  });
-});
