7137fdcd7519e4403c8e4e845bea8a04457c7efe
Utlwang Mooka <123069417+Braidy1@users.noreply.github.com>
2026-01-22 23:16:55 +0200

    Delete engine directory

--- engine/ghosts.js
+++
@@ -1,6 +0,0 @@
-export const ghosts=[
-  { id:'compute', name:'Azure Compute', color:'#0078D4', x:200, y:200 },
-  { id:'data', name:'Azure Data', color:'#00B294', x:260, y:200 },
-  { id:'security', name:'Azure Security', color:'#68217A', x:230, y:260 },
-  { id:'devops', name:'Azure DevOps', color:'#F25022', x:290, y:260 }
-];
